6c895e1f74ec24fcc2e916ee364a92f71d23f89a
Chris Kirk <chris@parabola.io>
2019-01-29 16:32:45 -0800

    adding static favicon.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const serverless = require('serverless-http');
 const bodyParser = require('body-parser');
 const path = require('path');
 const express = require('express');
+const favicon = require('serve-favicon');
 
 const balls = require('./api/balls');
 const bones = require('./api/bones');
@@ -9,9 +10,10 @@ const holes = require('./api/holes');
 
 const app = express();
 app.use(bodyParser.json({ strict: false }));
+app.use(favicon(path.join(__dirname, 'static', 'images', 'favicon.ico')));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'static/index.html'));
+  res.sendFile(path.join(__dirname, 'static', 'index.html'));
 });
 
 app.use('/api/balls', balls);
